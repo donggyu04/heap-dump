f93a6f1d2430a3ef79146f884dcd5631b46cadd6
donggyu.lee <donggyu.lee@nts-corp.com>
2020-04-05 01:22:14 +0900

    Refine OOM generation logic

--- src/main/java/com/training/dumptest/model/OOMGenerator.java
+++ src/main/java/com/training/dumptest/model/OOMGenerator.java
@@ -5,23 +5,21 @@ import org.slf4j.LoggerFactory;
 
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Long.SIZE;
+import static java.lang.Math.abs;
 import static java.lang.Runtime.getRuntime;
 
 public class OOMGenerator {
     private Logger logger = LoggerFactory.getLogger(OOMGenerator.class);
 
     public void generateOOM() throws Exception {
-        int dummyArraySize = 15;
-        long memoryConsumed = 0;
-        long[] memoryAllocated;
+        int arraySize = 5;
 
         logger.info("Max JVM memory: " + getRuntime().maxMemory());
 
         for (int loop = 0; loop < MAX_VALUE; loop++) {
-            memoryAllocated = new long[dummyArraySize];
-            memoryConsumed += dummyArraySize * SIZE;
-            logger.info("Memory Consumed till now: " + memoryConsumed);
-            dummyArraySize *= dummyArraySize * 2;
+            long[] memoryAllocated = new long[abs(arraySize)];
+            logger.info("Current allocation memory : " + abs(arraySize * SIZE));
+            arraySize *= 10;
             Thread.sleep(1000);
         }
     }
